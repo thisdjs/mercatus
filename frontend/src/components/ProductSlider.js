0cdda7644cd2a875f53ebb912f0106739bc3e9f3
david <this.djs.19@gmail.com>
2021-02-18 22:03:54 +0000

    fix ssr carousel

--- frontend/src/components/ProductSlider.js
+++ frontend/src/components/ProductSlider.js
@@ -38,22 +38,6 @@ function ProductSlider() {
   const isTabletOrMobile = useMediaQuery({ maxWidth: 1024 });
   return (
     <InfiniteCarousel
-      breakpoints={[
-        {
-          breakpoint: 640,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          },
-        },
-      ]}
       showSides={isTabletOrMobile}
       sidesOpacity={0.5}
       sideSize={0.1}
